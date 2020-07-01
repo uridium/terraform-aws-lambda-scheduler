feb3700fd1e32b8d326b0c4f89399bb8c99dba03
Radar <radar@protonmail.com>
2020-07-01 08:47:56 +0200

    Add IAM role description

--- main.tf
+++ main.tf
@@ -34,6 +34,7 @@ data "archive_file" "layer_zip" {
 
 resource "aws_iam_role" "this" {
   name               = local.name
+  description        = var.description
   assume_role_policy = data.aws_iam_policy_document.this.json
 }
 
