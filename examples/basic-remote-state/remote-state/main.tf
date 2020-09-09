e5d49f372246f5fc8e7803b8e3c90acad9f47ea1
Radar <radar@protonmail.com>
2020-09-09 11:49:25 +0200

    Add aws_s3_bucket encryption

--- examples/basic-remote-state/remote-state/main.tf
+++ examples/basic-remote-state/remote-state/main.tf
@@ -9,6 +9,14 @@ locals {
 resource "aws_s3_bucket" "this" {
   bucket = local.name
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   versioning {
     enabled = true
   }
