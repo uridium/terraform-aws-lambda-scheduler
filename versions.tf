38252368c7511285f9ff077b52ae2e654eef5519
Radar <radar@protonmail.com>
2021-07-27 18:18:29 +0200

    Use terraform >= 1.0

--- versions.tf
+++ versions.tf
@@ -1,8 +1,14 @@
 terraform {
-  required_version = "~> 0.13.2"
+  required_version = "~> 1.0"
 
   required_providers {
-    aws     = "~> 3.5.0"
-    archive = "~> 1.3.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.51.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.2.0"
+    }
   }
 }
