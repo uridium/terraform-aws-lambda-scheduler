de241d32956f6991e5217907927761c57e794a6d
Radar <radar@protonmail.com>
2021-11-04 14:35:09 +0100

    Add tags to IAM role

--- main.tf
+++ main.tf
@@ -39,6 +39,7 @@ resource "aws_iam_role" "this" {
   name               = local.name
   description        = var.description
   assume_role_policy = data.aws_iam_policy_document.this.json
+  tags               = var.tags
 }
 
 resource "aws_iam_role_policy_attachment" "lambda" {
