b0cce1eb64daf2639e6e3eb65683a873ca9c2f38
Radar <radar@protonmail.com>
2023-05-01 12:27:19 +0200

    Remove lambda_layer_filename output

--- outputs.tf
+++ outputs.tf
@@ -18,11 +18,6 @@ output "lambda_function_filename" {
   value       = aws_lambda_function.this.filename
 }
 
-output "lambda_layer_filename" {
-  description = "The filename of the layer zip archive"
-  value       = aws_lambda_layer_version.this.*.filename
-}
-
 output "lambda_function_handler" {
   description = "The filename.handler-method value in your function"
   value       = aws_lambda_function.this.handler
